7f977a2a29f7ef11028ed11c78b16ec76f91a312
myf@worldtalks.org <myf@ctesiphon.(none)>
2012-03-24 15:54:46 -0400

    add event interaction

--- socket.js
+++ socket.js
@@ -1,7 +1,8 @@
 var server = require('http').createServer(handler),
     io = require('socket.io').listen(server),
     fs = require('fs'),
-    pwd  = process.cwd();
+    pwd  = process.cwd(),
+    exec = require('child_process').exec;
 
 server.listen(3000);
 
@@ -18,11 +19,38 @@ function handler(req, res){
         res.end(data);
     });
 }
+//getting output from a command issued
+
+/*
+function command(com,callback) {
+    var html;
+    exec(com, function(error, stdout, stderr){
+    var child = stdout;
+    var raw = child.match(/^.*([]+|$)/gm);
+    var len = raw.length;
+    var arr = new Array(len);
+    for (i=0;i<=len;i++){
+        arr[i] = "<br><a href = '"+raw[i] + "'>" + raw[i] +"</a>";
+    }
+    html = arr.join("\n");
+    console.log("this is command "+html);
+    callback(html);
+
+    });
+};
+*/
 
 io.sockets.on('connection', function(socket){
-    socket.emit('news', {hello: 'world'});
-    socket.on('something else', function(data){
-        var command = data.data;
-        console.log(command);
+    //socket.emit('news', {hello: 'world'});
+    socket.on('enter', function(data){
+        exec(data, function (error, stdout, stderr){
+        console.log(data.length);
+        var output = stdout;
+        var output = output.match(/^.*([]+|$)/gm);
+        console.log(data);
+        console.log(stderr);
+        console.log(output);
+        });
+        //socket.broadcast.emit(output);
     });
 });
